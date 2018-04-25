f5a0784577c0a66f1ba35c3894cae8c342a342de
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-25 14:20:24 -0300

    Repeating * in lines

--- src/com/introduction/Main.java
+++ src/com/introduction/Main.java
@@ -3,14 +3,11 @@ package com.introduction;
 public class Main {
 
     public static void main(String[] args) {
-        int numb = 8;
+        int numb = 3;
         int a = 0;
         while (a < numb) {
-            System.out.print("*");
+            System.out.println("*");
             a++;
         }
-
-
-
     }
 }
