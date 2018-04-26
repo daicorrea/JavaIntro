6970bf42ac288a6763665678ad19be8f37d27154
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-26 13:06:52 -0300

    Using repeat

--- src/com/introduction/Main.java
+++ src/com/introduction/Main.java
@@ -5,19 +5,21 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int lines_qty = 4;
+        int lines_qty = 5;
         int whitespaces = lines_qty - 1;
+        int triangle_line = 1;
         for (int i = 0; i < lines_qty; i++) {
-            for (int j = 0; j <= lines_qty + 2; j++) {
-                if (j > whitespaces && j <= ((lines_qty + 2) - whitespaces)) {
-                    System.out.print("*");
-                }
-                else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println("\n");
+            System.out.print(repeat(" ", whitespaces));
+            System.out.print(repeat("*",triangle_line));
+            System.out.print("\n");
+            triangle_line += 2;
             whitespaces--;
         }
+    }
+
+    public static String repeat (String str_to_repeat, int times_to_repeat) {
+        char[] repeating = new char[times_to_repeat];
+        String repeated = String.valueOf(repeating).replace("\0", str_to_repeat);
+        return repeated;
     }
 }
