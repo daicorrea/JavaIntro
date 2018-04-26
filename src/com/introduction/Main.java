f472d5c4346e7c6173a16b2ddc97220a4ce41608
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-26 10:01:20 -0300

    Using matrix

--- src/com/introduction/Main.java
+++ src/com/introduction/Main.java
@@ -1,13 +1,24 @@
 package com.introduction;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         int numb = 3;
-        int a = 0;
-        while (a < numb) {
-            System.out.println("*");
-            a++;
+        int whitespaces = numb-1;
+        char triangle_field[][] = new char[numb][numb+2];
+        for (int i = 0; i < numb; i++) {
+            for (int j = 0; j < numb + 2; j++) {
+                if((j > whitespaces) && (j < (numb + 2) - whitespaces)) {
+                    triangle_field[i][j] = ' ';
+                }
+                else {
+                    triangle_field[i][j] = '*';
+                }
+            }
+            whitespaces--;
         }
+        System.out.println(Arrays.deepToString(triangle_field));
     }
 }
