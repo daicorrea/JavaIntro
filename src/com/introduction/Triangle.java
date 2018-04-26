8beebb44a71abf4a7e64c6a0d0f10a074d620582
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-26 13:11:06 -0300

    Changing Names

--- src/com/introduction/Triangle.java
+++ src/com/introduction/Triangle.java
@@ -1,8 +1,6 @@
 package com.introduction;
 
-import java.util.Arrays;
-
-public class Main {
+public class Triangle {
 
     public static void main(String[] args) {
         int lines_qty = 10;
@@ -14,7 +12,7 @@ public class Main {
             whitespaces--;
         }
     }
-    
+
     public static String repeat (String str_to_repeat, int times_to_repeat) {
         char[] repeating = new char[times_to_repeat];
         String repeated = String.valueOf(repeating).replace("\0", str_to_repeat);
