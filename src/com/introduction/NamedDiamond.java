f1147ce6a095cd2c9134a182ec27349fa9878100
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-26 14:06:39 -0300

    Adding Named Diamond

--- src/com/introduction/NamedDiamond.java
+++ src/com/introduction/NamedDiamond.java
@@ -1,18 +1,18 @@
 package com.introduction;
 
-public class Diamond {
+public class NamedDiamond {
 
     public static void main(String[] args) {
-        int lines_qty = 3;
+        int lines_qty = 5;
         int whitespaces = lines_qty - 1;
         int triangle_line = 1;
-        for (int i = 0; i < lines_qty; i++) {
+        for (int i = 0; i < lines_qty - 1; i++) {
             System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
             triangle_line += 2;
             whitespaces--;
         }
-        triangle_line -= 4;
-        whitespaces++;
+        System.out.println("Daiane");
+        triangle_line -= 2;
         for (int i = lines_qty; i > 1; i--) {
             whitespaces++;
             System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
