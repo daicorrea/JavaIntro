f730cf111e1ac3147414ca0f099cbae6ababd2cf
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-26 13:56:52 -0300

    Creating Diamond

--- src/com/introduction/Diamond.java
+++ src/com/introduction/Diamond.java
@@ -3,13 +3,20 @@ package com.introduction;
 public class Diamond {
 
     public static void main(String[] args) {
-        int lines_qty = 10;
+        int lines_qty = 1;
         int whitespaces = lines_qty - 1;
         int triangle_line = 1;
         for (int i = 0; i < lines_qty; i++) {
             System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
             triangle_line += 2;
             whitespaces--;
+        }
+        triangle_line -= 4;
+        whitespaces++;
+        for (int i = lines_qty; i > 1; i--) {
+            whitespaces++;
+            System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
+            triangle_line -= 2;
         }
     }
 
@@ -18,4 +25,5 @@ public class Diamond {
         String repeated = String.valueOf(repeating).replace("\0", str_to_repeat);
         return repeated;
     }
+
 }
