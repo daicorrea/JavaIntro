166b9a0e79dc7e31903ca8126f2de75d92168e04
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-27 17:25:32 -0300

    Dividing Diamond in Methods

--- src/com/introduction/Diamond.java
+++ src/com/introduction/Diamond.java
@@ -1,29 +1,53 @@
 package com.introduction;
 
-public class Diamond {
+public class Triangle {
 
     public static void main(String[] args) {
         int lines_qty = 3;
-        int whitespaces = lines_qty - 1;
-        int triangle_line = 1;
-        for (int i = 0; i < lines_qty; i++) {
-            System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
-            triangle_line += 2;
-            whitespaces--;
-        }
-        triangle_line -= 4;
-        whitespaces++;
-        for (int i = lines_qty; i > 1; i--) {
-            whitespaces++;
-            System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
-            triangle_line -= 2;
-        }
+        int whitespaces_qty = getWhitespaceQuantityByLine(lines_qty);
+        System.out.println(drawDiamond(lines_qty, whitespaces_qty));
     }
 
-    public static String repeat (String str_to_repeat, int times_to_repeat) {
+    public static String drawDiamond(int lines_qty, int whitespaces_qty) {
+        String diamondBottom = removeInitialLines(drawInvertedTriangle(lines_qty, whitespaces_qty), 2);
+
+        return (drawTriangle(lines_qty, whitespaces_qty)  +
+                diamondBottom);
+    }
+
+    public static String drawTriangle(int lines_qty, int whitespaces_qty) {
+        String triangle = new String();
+        int asterisks_qty = 1;
+        for (int i = 0; i < lines_qty; i++) {
+            triangle = triangle + repeatString(" ", whitespaces_qty)
+                    + repeatString("*",asterisks_qty) + repeatString(" ", whitespaces_qty) + '\n';
+            asterisks_qty += 2;
+            whitespaces_qty--;
+        }
+        return triangle;
+    }
+
+    public static String drawInvertedTriangle(int lines_qty, int whitespaces_qty) {
+        StringBuilder invertedTriangle = new StringBuilder();
+        invertedTriangle.append(drawTriangle(lines_qty, whitespaces_qty));
+        invertedTriangle.reverse();
+        return invertedTriangle.toString();
+    }
+
+    public static int getWhitespaceQuantityByLine(int lines_qty) {
+        return lines_qty - 1;
+    }
+
+    public static String repeatString(String str_to_repeat, int times_to_repeat) {
         char[] repeating = new char[times_to_repeat];
-        String repeated = String.valueOf(repeating).replace("\0", str_to_repeat);
-        return repeated;
+        return String.valueOf(repeating).replace("\0", str_to_repeat);
+    }
+
+    public static String removeInitialLines(String str_to_remove, int lines_qty) {
+        for(int i = 0; i < lines_qty; i++) {
+            str_to_remove = str_to_remove.substring(str_to_remove.indexOf("\n") + 1);
+        }
+        return str_to_remove;
     }
 
 }
