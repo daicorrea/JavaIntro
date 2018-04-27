b41f9d1d2dc816828576f38b3973d17afe6630c3
Daiane Correa <daiane.ocorrea@gmail.com>
2018-04-27 16:21:06 -0300

    Splitting code into methods

--- src/com/introduction/Triangle.java
+++ src/com/introduction/Triangle.java
@@ -3,19 +3,32 @@ package com.introduction;
 public class Triangle {
 
     public static void main(String[] args) {
-        int lines_qty = 10;
-        int whitespaces = lines_qty - 1;
-        int triangle_line = 1;
-        for (int i = 0; i < lines_qty; i++) {
-            System.out.print(repeat(" ", whitespaces) + repeat("*",triangle_line) + "\n");
-            triangle_line += 2;
-            whitespaces--;
-        }
+        int lines_qty = 3;
+        int whitespaces_qty = getWhitespaceQuantityByLine(lines_qty);
+        String triangle = drawTriangle(lines_qty, whitespaces_qty);
+        System.out.println(triangle);
     }
 
-    public static String repeat (String str_to_repeat, int times_to_repeat) {
-        char[] repeating = new char[times_to_repeat];
-        String repeated = String.valueOf(repeating).replace("\0", str_to_repeat);
-        return repeated;
+    public static String drawTriangle(int lines_qty, int whitespaces_qty) {
+        String triangle = new String();
+        int asterisks_qty = 1;
+        for (int i = 0; i < lines_qty; i++) {
+            triangle = triangle + repeatString(" ", whitespaces_qty)
+                    + repeatString("*",asterisks_qty) + '\n';
+            asterisks_qty += 2;
+            whitespaces_qty--;
+        }
+        return triangle;
     }
+
+    public static int getWhitespaceQuantityByLine(int lines_qty) {
+        return lines_qty - 1;
+    }
+
+    public static String repeatString (String str_to_repeat, int times_to_repeat) {
+        char[] repeating = new char[times_to_repeat];
+        return String.valueOf(repeating).replace("\0", str_to_repeat);
+    }
+
+
 }
