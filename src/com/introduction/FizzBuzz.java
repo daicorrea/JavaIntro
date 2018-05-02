458f54101c3fa214bfc5dad238eb0ac97412865a
Daiane Correa <daiane.ocorrea@gmail.com>
2018-05-02 15:32:39 -0300

    Instead of numbers divisible by three print 'Fizz'

--- src/com/introduction/FizzBuzz.java
+++ src/com/introduction/FizzBuzz.java
@@ -9,11 +9,19 @@ public class FizzBuzz {
     }
 
     public static ArrayList<String> getFizzBuzz() {
-        ArrayList<String> fizzBuzzList = new ArrayList<String>();
+        ArrayList<String> fizzBuzzList = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
-            fizzBuzzList.add(String.valueOf(i));
+            fizzBuzzList.add(verifyNumbers(i));
         }
         return fizzBuzzList;
+    }
+
+    public static String verifyNumbers(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        } else {
+            return String.valueOf(number);
+        }
     }
 
 }
